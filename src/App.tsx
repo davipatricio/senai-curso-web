4c4df320055d8e77846ec6663577e7c137753b01
Davi Patricio <davipatricio2006@gmail.com>
2024-04-26 19:00:11 +0000

    add products

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/header";
 import ProductSection from "./components/home/product-section";
-import { mostSelledProducts } from "./utils/products";
+import { mostSelledProducts, promoProducts } from "./utils/products";
 
 export default function App() {
   return (
@@ -11,7 +11,8 @@ export default function App() {
           a
         </aside>
 
-        <div className="flex w-full p-2 sm:p-4 md:p-10 h-fit">
+        <div className="flex flex-col gap-2 w-full p-2 sm:p-4 md:p-10 h-fit">
+          <ProductSection title="Em promoção" products={promoProducts} />
           <ProductSection title="Mais vendidos" products={mostSelledProducts} />
         </div>
       </main>
--- src/utils/products.ts
+++ src/utils/products.ts
@@ -1,10 +1,31 @@
 import { ProductData } from "./types";
 
 export const mostSelledProducts = [
-  ...Array.from({ length: 5 }).fill({
+  {
     name: "Headset Gamer Rise Mode G1, Drivers 50mm, P2, Preto - RM-HS-G1-FB",
     image:
       "https://images.kabum.com.br/produtos/fotos/483202/headset-gamer-rise-mode-g1-drivers-50mm-p2-preto-rm-hs-g1-fb_1712066527_gg.jpg",
     price: 179.99,
-  }),
+  },
+  {
+    name: "Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, US, Preto",
+    image:
+      "https://images.kabum.com.br/produtos/fotos/371586/teclado-mecanico-gamer-hyperx-alloy-mkw100-rgb-switch-red-full-size-layout-us-4p5e1aa-aba_1660832224_gg.jpg",
+    price: 349.99,
+  },
+] as ProductData[];
+
+export const promoProducts = [
+  {
+    name: "Mouse Gamer Redragon Cobra, Chroma RGB, 10000DPI, 7 Botões, Preto - M711 V2",
+    image:
+      "https://images.kabum.com.br/produtos/fotos/94555/mouse-gamer-redragon-cobra-chroma-rgb-12400dpi-7-botoes-preto-m711-v2_1656018617_gg.jpg",
+    price: 119.99,
+  },
+  {
+    name: "iPhone 15 Pro Max Apple 256GB, Câmera Dupla 48MP, Tela 6.7\", Preto Titânio",
+    image:
+      "https://images.kabum.com.br/produtos/fotos/486306/iphone-15-apple-pro-max-256gb-titanio-preto-tela-de-6-7-camera-dupla-de-48mp-ios-mu773be-a_1699651983_gg.jpg",
+    price: 11577.99,
+  },
 ] as ProductData[];
