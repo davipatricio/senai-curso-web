7e4510bd2cce6aeaf2176aed35b1174b5ac379d2
Davi Patricio <davipatricio2006@gmail.com>
2024-05-03 16:58:40 +0000

    make icon clickable

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,18 @@ export default function Header() {
   return (
     <header className="flex flex-wrap gap-4 p-4 justify-evenly items-center bg-neutral-950 border-b border-neutral-700 min-h-16">
       <div className="size-14 flex items-center justify-center">
-        <img src={Gopher} className="object-fill" />
+        <a href="/">
+          <img src={Gopher} className="object-fill" />
+        </a>
       </div>
       <nav className="flex flex-wrap justify-center gap-2 sm:text-lg">
         <Link className="font-bold hover:underline" to="/">
           Home
         </Link>
-        <Link className="font-bold hover:underline text-center" to="/cadastro-produtos">
+        <Link
+          className="font-bold hover:underline text-center"
+          to="/cadastro-produtos"
+        >
           Cadastro de Produtos
         </Link>
       </nav>
